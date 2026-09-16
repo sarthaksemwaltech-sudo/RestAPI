fe6ffa9baefc5c95287f9f55d80bbab2e78c1c00
Sarthak_Semwal <sarthak.semwal.tech@gmail.com>
2026-09-16 23:37:05 +0530

    Created-Student-Post-EndPoint

--- src/main/java/com/RestAPI/StudentManagementSystem/Controller/StudentController.java
+++ src/main/java/com/RestAPI/StudentManagementSystem/Controller/StudentController.java
@@ -1,8 +1,38 @@
 package com.RestAPI.StudentManagementSystem.Controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.RestAPI.StudentManagementSystem.DTO.StudentDTO;
+import com.RestAPI.StudentManagementSystem.Services.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/Students")
 public class StudentController {
+
+
+    //DI variable
+    private final StudentService studentService;
+
+
+    //Dependancy Injection
+    public StudentController(StudentService studentService) {
+
+        this.studentService=studentService;
+
+    }
+
+
+    //Endpoint to insert the student
+    @PostMapping()
+    public ResponseEntity<StudentDTO> insertStudent(@RequestBody StudentDTO studentDTO){
+
+        StudentDTO response=studentService.insertStudent(studentDTO);
+
+
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
+
+    }
+
 }
--- src/main/java/com/RestAPI/StudentManagementSystem/Services/StudentService.java
+++ src/main/java/com/RestAPI/StudentManagementSystem/Services/StudentService.java
@@ -1,7 +1,37 @@
 package com.RestAPI.StudentManagementSystem.Services;
 
+import com.RestAPI.StudentManagementSystem.DTO.StudentDTO;
+import com.RestAPI.StudentManagementSystem.Entity.StudentEntity;
+import com.RestAPI.StudentManagementSystem.Repository.StudentRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+
+    //Varible for the DI
+    private StudentRepository studentRepository;
+    private ModelMapper modelMapper;
+
+
+
+    //Dependancy injection
+
+    public StudentService(StudentRepository studentRepository, ModelMapper modelMapper) {
+        this.studentRepository = studentRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+
+    //Method to create the student inside the DB
+    public StudentDTO insertStudent(StudentDTO studentDTO) {
+
+       StudentEntity student=modelMapper.map(studentDTO,StudentEntity.class);
+
+       studentRepository.save(student);
+
+       return modelMapper.map(student,StudentDTO.class);
+    }
 }
