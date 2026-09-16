6fcf0228f79952d0a533f097c221088d9f70fc8e
Sarthak_Semwal <sarthak.semwal.tech@gmail.com>
2026-09-16 23:10:31 +0530

    Created-Controller-Folder

---
+++ src/main/java/com/RestAPI/StudentManagementSystem/Controller/AdmissionRecordController.java
@@ -0,0 +1,8 @@
+package com.RestAPI.StudentManagementSystem.Controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdmissionRecordController {
+}
