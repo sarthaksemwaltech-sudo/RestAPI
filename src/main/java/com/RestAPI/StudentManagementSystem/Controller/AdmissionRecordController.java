3cccf9bf00c983deed5b65226cf2ec5a0cb9e804
Sarthak_Semwal <sarthak.semwal.tech@gmail.com>
2026-09-17 23:09:15 +0530

    Created-AdmissionRecordCRUD-Endpoints

--- src/main/java/com/RestAPI/StudentManagementSystem/Controller/AdmissionRecordController.java
+++ src/main/java/com/RestAPI/StudentManagementSystem/Controller/AdmissionRecordController.java
@@ -1,8 +1,72 @@
 package com.RestAPI.StudentManagementSystem.Controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.RestAPI.StudentManagementSystem.DTO.AdmissionRecordDTO;
+import com.RestAPI.StudentManagementSystem.Services.AdmissionRecordService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/AdmissionRecords")
 public class AdmissionRecordController {
+
+
+    //DI variable
+    private final AdmissionRecordService admissionRecordService;
+
+
+    //Dependancy Injection
+    public AdmissionRecordController(AdmissionRecordService admissionRecordService) {
+
+        this.admissionRecordService = admissionRecordService;
+
+    }
+
+
+    //Endpoint to insert the admission record
+    @PostMapping()
+    public ResponseEntity<AdmissionRecordDTO> insertAdmissionRecord(@RequestBody AdmissionRecordDTO admissionRecordDTO){
+
+        AdmissionRecordDTO response = admissionRecordService.insertAdmissionRecord(admissionRecordDTO);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+
+    }
+
+
+    //EndPoint to Get All Admission Records
+
+    @GetMapping()
+    public ResponseEntity<List<AdmissionRecordDTO>> getAllAdmissionRecords(){
+
+        return new ResponseEntity<>(admissionRecordService.getAllAdmissionRecordService(), HttpStatus.OK);
+    }
+
+
+    //Endpoint to get the admission record by ID
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<AdmissionRecordDTO> getAdmissionRecordbyID(@PathVariable Long id){
+
+        return new ResponseEntity<>(admissionRecordService.getAdmissionRecordByIdService(id), HttpStatus.OK);
+    }
+
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<AdmissionRecordDTO> updateAdmissionRecord(@PathVariable Long id, @RequestBody AdmissionRecordDTO admissionRecordDTO){
+
+        return new ResponseEntity<>(admissionRecordService.updateAdmissionRecordService(id, admissionRecordDTO), HttpStatus.OK);
+
+    }
+
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Boolean> deleteAdmissionRecord(@PathVariable Long id){
+
+        return new ResponseEntity<>(admissionRecordService.deleteAdmissionRecordService(id), HttpStatus.ACCEPTED);
+    }
+
 }
--- src/main/java/com/RestAPI/StudentManagementSystem/Services/AdmissionRecordService.java
+++ src/main/java/com/RestAPI/StudentManagementSystem/Services/AdmissionRecordService.java
@@ -1,8 +1,91 @@
 package com.RestAPI.StudentManagementSystem.Services;
 
-
+import com.RestAPI.StudentManagementSystem.DTO.AdmissionRecordDTO;
+import com.RestAPI.StudentManagementSystem.Entity.AdmissionRecordEntity;
+import com.RestAPI.StudentManagementSystem.Repository.AdmissionRecordRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class AdmissionRecordService {
+
+
+    //Varible for the DI
+    private AdmissionRecordRepository admissionRecordRepository;
+    private ModelMapper modelMapper;
+
+
+
+    //Dependancy injection
+
+    public AdmissionRecordService(AdmissionRecordRepository admissionRecordRepository, ModelMapper modelMapper) {
+        this.admissionRecordRepository = admissionRecordRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+
+    //Method to create the admission record inside the DB
+    public AdmissionRecordDTO insertAdmissionRecord(AdmissionRecordDTO admissionRecordDTO) {
+
+        AdmissionRecordEntity admissionRecord = modelMapper.map(admissionRecordDTO, AdmissionRecordEntity.class);
+
+        admissionRecordRepository.save(admissionRecord);
+
+        return modelMapper.map(admissionRecord, AdmissionRecordDTO.class);
+    }
+
+    public List<AdmissionRecordDTO> getAllAdmissionRecordService() {
+
+        List<AdmissionRecordEntity> admissionRecords = admissionRecordRepository.findAll();
+
+        return admissionRecords.
+                stream().
+                map(admissionRecord -> modelMapper.map(admissionRecord, AdmissionRecordDTO.class)).
+                toList();
+    }
+
+
+    //Method to Get Admission Record By ID
+
+    public AdmissionRecordDTO getAdmissionRecordByIdService(Long id) {
+
+        AdmissionRecordEntity admissionRecord = admissionRecordRepository.findById(id).orElseThrow();
+
+        return modelMapper.map(admissionRecord, AdmissionRecordDTO.class);
+
+
+    }
+
+
+    //Method to Update Admission Record
+    public AdmissionRecordDTO updateAdmissionRecordService(Long id, AdmissionRecordDTO admissionRecordDTO) {
+
+        AdmissionRecordEntity admissionRecord = modelMapper.map(admissionRecordDTO, AdmissionRecordEntity.class);
+
+        admissionRecord.setId(id);
+
+        admissionRecordRepository.save(admissionRecord);
+
+
+        return modelMapper.map(admissionRecord, AdmissionRecordDTO.class);
+
+
+    }
+
+
+    //Method to Delete Admission Record By ID
+
+    public Boolean deleteAdmissionRecordService(Long id) {
+        if(!admissionRecordRepository.existsById(id)) {
+            throw new NoSuchElementException("Entity Not Found");
+        }
+
+        admissionRecordRepository.deleteById(id);
+
+        return true;
+    }
 }
