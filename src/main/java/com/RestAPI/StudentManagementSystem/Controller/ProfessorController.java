f6bfe13a7df21ebbb50536609dcad814626c9d8d
Sarthak_Semwal <sarthak.semwal.tech@gmail.com>
2026-09-17 22:36:06 +0530

    Created-ProfessorCRUD-Endpoints

--- src/main/java/com/RestAPI/StudentManagementSystem/Controller/ProfessorController.java
+++ src/main/java/com/RestAPI/StudentManagementSystem/Controller/ProfessorController.java
@@ -1,8 +1,85 @@
 package com.RestAPI.StudentManagementSystem.Controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.RestAPI.StudentManagementSystem.DTO.ProfessorDTO;
+import com.RestAPI.StudentManagementSystem.DTO.StudentDTO;
+import com.RestAPI.StudentManagementSystem.Services.ProfessorService;
+import com.RestAPI.StudentManagementSystem.Services.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/professor")
 public class ProfessorController {
+
+    private final ProfessorService professorService;
+
+
+    //Dependancy Injection
+    public ProfessorController(ProfessorService professorService) {
+
+      this.professorService = professorService;
+
+    }
+
+
+    //Endpoint to insert the student
+    @PostMapping()
+    public ResponseEntity<ProfessorDTO> insertprofessor(@RequestBody ProfessorDTO professorDTO){
+
+        ProfessorDTO response=professorService.insertProfessorService(professorDTO);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+
+    }
+
+
+    //EndPoint to Get All Students
+
+    @GetMapping()
+    public ResponseEntity<List<ProfessorDTO>> getAllProfessor(){
+
+        return new ResponseEntity<>(professorService.getAllProfessorService(),HttpStatus.OK);
+    }
+
+
+    //Endpoint to get the student by ID
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProfessorDTO>getProfessorbyID(@PathVariable Long id){
+
+        return new ResponseEntity<>(professorService.getProfessorByIdService(id), HttpStatus.OK);
+    }
+
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProfessorDTO>updateProfessor(@PathVariable Long id,@RequestBody ProfessorDTO professorDTO){
+
+        return new ResponseEntity<>(professorService.updateProfessorService(id,professorDTO),HttpStatus.OK);
+
+    }
+
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Boolean>deleteProfessor(@PathVariable Long id){
+
+        return new ResponseEntity<>(professorService.deleteProfessorService(id),HttpStatus.ACCEPTED);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/RestAPI/StudentManagementSystem/Services/ProfessorService.java
+++ src/main/java/com/RestAPI/StudentManagementSystem/Services/ProfessorService.java
@@ -1,7 +1,91 @@
 package com.RestAPI.StudentManagementSystem.Services;
 
+import com.RestAPI.StudentManagementSystem.DTO.ProfessorDTO;
+import com.RestAPI.StudentManagementSystem.DTO.StudentDTO;
+import com.RestAPI.StudentManagementSystem.Entity.ProfessorEntity;
+import com.RestAPI.StudentManagementSystem.Entity.StudentEntity;
+import com.RestAPI.StudentManagementSystem.Repository.ProfessorRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class ProfessorService {
+
+    private ProfessorRepository professorRepository;
+
+    private ModelMapper modelMapper;
+
+
+    public ProfessorService(ProfessorRepository professorRepository,ModelMapper modelMapper) {
+
+        this.professorRepository = professorRepository;
+        this.modelMapper=modelMapper;
+    }
+
+
+
+    public ProfessorDTO insertProfessorService(ProfessorDTO professorDTO) {
+
+        ProfessorEntity professor=modelMapper.map(professorDTO,ProfessorEntity.class);
+
+        professorRepository.save(professor);
+
+        return modelMapper.map(professor, ProfessorDTO.class);
+
+    }
+
+
+
+
+    public List<ProfessorDTO> getAllProfessorService() {
+
+        List<ProfessorEntity>professors=professorRepository.findAll();
+
+        return professors.
+                stream().
+                map(professor->modelMapper.map(professor,ProfessorDTO.class)).
+                toList();
+    }
+
+
+
+    public ProfessorDTO getProfessorByIdService(Long id) {
+
+        ProfessorEntity professor=professorRepository.findById(id).orElseThrow();
+
+        return modelMapper.map(professor,ProfessorDTO.class);
+    }
+
+
+
+    public ProfessorDTO updateProfessorService(Long id, ProfessorDTO professorDTO) {
+
+        ProfessorEntity professor =modelMapper.map(professorDTO,ProfessorEntity.class);
+
+        professor.setId(id);
+
+        professorRepository.save(professor);
+
+
+        return modelMapper.map(professor,ProfessorDTO.class);
+    }
+
+
+
+    public Boolean deleteProfessorService(Long id) {
+
+        if(!professorRepository.existsById(id)) {
+            throw new NoSuchElementException("Entity Not Found");
+        }
+
+        professorRepository.deleteById(id);
+
+        return true;
+    }
+
+
+
 }
